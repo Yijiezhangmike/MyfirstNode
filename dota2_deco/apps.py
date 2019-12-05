109ea8795c0b8ee9aec490f0eda93d9afdc2fe3b
Yijiezhangmike <1196509384@qq.com>
2019-12-05 14:53:53 -0800

    dota2 decoration

---
+++ dota2_deco/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Dota2DecoConfig(AppConfig):
+    name = 'dota2_deco'
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/apple/Desktop/use_this_for_mvc/mvc-20191117/project/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
